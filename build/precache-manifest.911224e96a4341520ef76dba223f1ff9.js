3cf91a86e537735296bf683db0f2a884c103b2c3
RiKu95 <rijulkumar3995@gmail.com>
2019-02-08 21:34:50 +0400

    skeleton for creating profile page using drawer without state

---
+++ build/precache-manifest.911224e96a4341520ef76dba223f1ff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7049544e68a779b6474",
+    "url": "/RiKu95/profile-temp/static/css/main.c6e5dc7e.chunk.css"
+  },
+  {
+    "revision": "f7049544e68a779b6474",
+    "url": "/RiKu95/profile-temp/static/js/main.f7049544.chunk.js"
+  },
+  {
+    "revision": "1ca4377a0c8c01aee7d6",
+    "url": "/RiKu95/profile-temp/static/css/1.0549756e.chunk.css"
+  },
+  {
+    "revision": "1ca4377a0c8c01aee7d6",
+    "url": "/RiKu95/profile-temp/static/js/1.1ca4377a.chunk.js"
+  },
+  {
+    "revision": "11fbfd0273b0f3382e95",
+    "url": "/RiKu95/profile-temp/static/js/runtime~main.11fbfd02.js"
+  },
+  {
+    "revision": "06472e8713bfe4512fb82f83f2274bdd",
+    "url": "/RiKu95/profile-temp/index.html"
+  }
+];
